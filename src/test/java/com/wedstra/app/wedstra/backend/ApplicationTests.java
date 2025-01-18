8133d79b3d403bacf536d629c77ada46d1200fc3
Yuvraj-Lolage <yuvilolage7@gmail.com>
2025-01-19 02:35:53 +0530

    first commit

---
+++ src/test/java/com/wedstra/app/wedstra/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedstra.app.wedstra.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
