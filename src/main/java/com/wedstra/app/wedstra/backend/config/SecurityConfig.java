18cbe7e96bb27c1912502dfef8129ba46ab6eca2
Yuvraj-Lolage <yuvilolage7@gmail.com>
2025-03-22 01:27:50 +0530

    added Cros Origin Policy

--- src/main/java/com/wedstra/app/wedstra/backend/config/SecurityConfig.java
+++ src/main/java/com/wedstra/app/wedstra/backend/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://wedstra-frontend.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
